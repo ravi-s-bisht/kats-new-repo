664d531165736b9c9e8d5d5c77589ecdb5714c2b
Nahom4258 <nhabtamu42@gmail.com>
2024-11-20 00:00:32 +0300

    avatarx dailybots demo

---
+++ app/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { DailyTransport } from "@daily-co/realtime-ai-daily";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { useEffect, useRef, useState } from "react";
+import { LLMHelper, RTVIClient } from "realtime-ai";
+import { RTVIClientAudio, RTVIClientProvider } from "realtime-ai-react";
+
+import App from "@/components/App";
+import { AppProvider } from "@/components/context";
+import Header from "@/components/Header";
+import Splash from "@/components/Splash";
+import {
+  BOT_READY_TIMEOUT,
+  defaultConfig,
+  defaultServices,
+} from "@/rtvi.config";
+
+export default function Home() {
+  const [showSplash, setShowSplash] = useState(true);
+  const voiceClientRef = useRef<RTVIClient | null>(null);
+
+  useEffect(() => {
+    if (!showSplash) {
+      return;
+    }
+
+    const voiceClient = new RTVIClient({
+      transport: new DailyTransport(),
+      params: {
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "/api",
+        requestData: {
+          services: defaultServices,
+          config: defaultConfig,
+        },
+      },
+      timeout: BOT_READY_TIMEOUT,
+    });
+
+    const llmHelper = new LLMHelper({});
+    voiceClient.registerHelper("llm", llmHelper);
+
+    voiceClientRef.current = voiceClient;
+  }, [showSplash]);
+
+  if (showSplash) {
+    return <Splash handleReady={() => setShowSplash(false)} />;
+  }
+
+  return (
+    <RTVIClientProvider client={voiceClientRef.current!}>
+      <AppProvider>
+        <TooltipProvider>
+          <main>
+            <Header />
+            <div id="app">
+              <App setShowSplash={setShowSplash} />
+            </div>
+          </main>
+          <aside id="tray" />
+        </TooltipProvider>
+      </AppProvider>
+      <RTVIClientAudio />
+    </RTVIClientProvider>
+  );
+}
---
+++ components/Splash.tsx
@@ -0,0 +1,58 @@
+import { Book, Info } from "lucide-react";
+import React from "react";
+
+import { Button } from "./ui/button";
+
+type SplashProps = {
+  handleReady: () => void;
+};
+
+export const Splash: React.FC<SplashProps> = ({ handleReady }) => {
+  return (
+    <main className="w-full flex items-center justify-center bg-primary-200 p-4 bg-[length:auto_50%] lg:bg-auto bg-colorWash bg-no-repeat bg-right-top">
+      <div className="flex flex-col gap-8 lg:gap-12 items-center max-w-full lg:max-w-3xl">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-balance text-left">
+          AvatarX demo
+        </h1>
+{/* 
+        <p className="text-primary-500 text-lg font-semibold leading-relaxed">
+          Anthropic Claude 3.5 Sonnet <br />
+          Llama 3.1 8B, Llama 3.1 70B, Llama 3.1 405B <br />
+          Open AI GPT-4o, GPT-4o mini
+          <br />
+          Grok AI Beta
+          <br />
+          Gemini 1.5 Flash, 1.0 Pro
+        </p> */}
+
+        <Button onClick={() => handleReady()}>Try Demo</Button>
+
+        <div className="h-[1px] bg-primary-300 w-full" />
+
+        {/* <footer className="flex flex-col lg:gap-2">
+          <Button variant="light" asChild>
+            <a
+              href="https://www.daily.co/products/daily-bots/"
+              className="text-indigo-600"
+            >
+              <Info className="size-6" />
+              Learn more about Daily Bots
+            </a>
+          </Button>
+
+          <Button variant="light" asChild>
+            <a
+              href="https://github.com/daily-demos/daily-bots-web-demo"
+              className="text-indigo-600"
+            >
+              <Book className="size-6" />
+              Demo source code
+            </a>
+          </Button>
+        </footer> */}
+      </div>
+    </main>
+  );
+};
+
+export default Splash;
