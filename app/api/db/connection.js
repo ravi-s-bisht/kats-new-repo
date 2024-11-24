fb4597e709e0c752ad1e0af0c9ff3ee8c6c5a02a
Nahom4258 <nhabtamu42@gmail.com>
2024-11-24 21:56:11 +0300

    fix knex issue

--- app/api/db/connection.js
+++ app/api/db/connection.js
@@ -8,7 +8,9 @@ const db = Knex({
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASS,
         database: process.env.MYSQL_DB_DEMO,
-        ssl: true,
+        ssl: {
+            rejectUnauthorized: true // Defaults to true, enforces secure connection
+        },
     },
 })
 
