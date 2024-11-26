8fcbba04fa19b7683a524d3cab900315ce50277f
Nahom4258 <nhabtamu42@gmail.com>
2024-11-26 20:01:06 +0300

    change voice to Mac

--- rtvi.config.ts
+++ rtvi.config.ts
@@ -9,7 +9,7 @@ export const LANGUAGES = [
     value: "en",
     tts_model: "eleven_turbo_v2_5",
     stt_model: "nova-2-general",
-    default_voice: "MXGyTMlsvQgQ4BL0emIa",
+    default_voice: "I33geqnOHQGKDPUMUspQ",
   },
   {
     label: "Spanish",
@@ -94,7 +94,7 @@ export const defaultConfig = [
   {
     service: "tts",
     options: [
-      { name: "voice", value: "MXGyTMlsvQgQ4BL0emIa" },
+      { name: "voice", value: "I33geqnOHQGKDPUMUspQ" },
       { name: "model", value: LANGUAGES[0].tts_model },
       { name: "language", value: LANGUAGES[0].value },
       {
