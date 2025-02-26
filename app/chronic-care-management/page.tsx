8d33777e7752f2b734e8ccef39de2f901c7c5395
ravindra <ravindra>
2025-02-26 11:11:19 +0530

    added new page

---
+++ app/chronic-care-management/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Chronic Care Management</h1>
+        </div>
+    );
+}
+
+export default Page;
---
+++ app/remote-patient-monitoring/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RemotePatientMonitoring = () => {
+  return (
+    <div>
+      <h1>Remote Patient Monitoring</h1>
+    </div>
+  );
+};
+
+export default RemotePatientMonitoring;
+
+
